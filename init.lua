439d0e6ac8838c309ef71ab434c604937443b133
Wesley Robb <wesrobb86@gmail.com>
2025-11-20 09:18:00 +0200

    Add fugitive

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ vim.pack.add {
     { src = 'https://github.com/nvim-mini/mini.files' },
     { src = 'https://github.com/folke/which-key.nvim' },
     { src = 'https://github.com/numToStr/Comment.nvim' },
+    { src = 'https://github.com/tpope/vim-fugitive' },
 }
 
 -- Setup colorscheme
